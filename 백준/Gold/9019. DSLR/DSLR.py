128dee6310ecfd1da60d773e0a20665e10015446
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2024-01-06 20:52:18 +0900

    [Gold IV] Title: DSLR, Time: 5076 ms, Memory: 214128 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -3,31 +3,46 @@ from collections import deque
 
 T = int(input())
 
+def D(n):
+    return (n*2) % 10000
+
+
+def S(n):
+    return (n-1) % 10000
+
+
+def L(n):
+    return (((n % 1000) // 100)*1000) + (((n % 100) // 10) * 100) + ((n % 10) * 10) + n // 1000
+
+
+def R(n):
+    return (n % 10) * 1000 + (n // 1000) * 100 + ((n % 1000) // 100) * 10 + ((n % 100) // 10)
+
+
 for _ in range(T):
     A, B = map(int, sys.stdin.readline().rstrip().split())
     queue = deque()
-    queue.append([A, ''])
-    visited = [False for i in range(10001)]
-    visited[A] = True
+    queue.append((A, ''))
+    checked = [False for _ in range(10001)]
 
-    while queue:
-        num, text = queue.popleft()
-        if (num == B):
+    while (queue):
+        (N, text) = queue.popleft()
+        if (N == B):
             print(text)
             break
-        d = (num*2)%10000
-        if not visited[d]:
-            visited[d] = True
-            queue.append([d, text+'D'])
-        s = (num-1)%10000
-        if not visited[s]:
-            visited[s] = True
-            queue.append([s, text+'S'])
-        l = num // 1000 + (num % 1000)*10
-        if not visited[l]:
-            visited[l] = True
-            queue.append([l, text+'L'])
-        r = num // 10 + (num % 10) * 1000
-        if not visited[r]:
-            visited[r] = True
-            queue.append([r, text+'R'])
+        d = D(N)
+        if not checked[d]:
+            checked[d] = True
+            queue.append((d, text+'D'))
+        s = S(N)
+        if not checked[s]:
+            checked[s] = True
+            queue.append((s, text+'S'))
+        l = L(N)
+        if not checked[l]:
+            checked[l] = True
+            queue.append((l, text+'L'))
+        r = R(N)
+        if not checked[r]:
+            checked[r] = True
+            queue.append((r, text+'R'))
