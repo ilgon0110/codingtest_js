41dd39b6d8889a1a0512c506ed2cc7b6692ba159
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2024-01-09 14:54:12 +0900

    [Silver I] Title: 절댓값 힙, Time: 144 ms, Memory: 39824 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+queue = []
+for _ in range(N):
+    x = int(sys.stdin.readline())
+
+    if x == 0:
+        if len(queue) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(queue)[1])
+    else:
+        heapq.heappush(queue, (abs(x), x))
