9adc419f6849ede11d7e92583e0a1c1433734673
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2024-03-09 16:28:20 +0900

    [Gold III] Title: 치즈, Time: 1236 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,54 +1,55 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
-
 board = []
-
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 for _ in range(N):
     board.append(list(map(int, input().split())))
 
-flag = True
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-ans = 0
 
-def isAround(i, j):
+def isOutSide(board):
     queue = deque()
-    queue.append((i, j))
-    board[i][j] = 2
+    queue.append((0, 0))
+    board[0][0] = 2
 
     while queue:
         x, y = queue.popleft()
         for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
+            nx = x+dx[k]
+            ny = y+dy[k]
             if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
                 board[nx][ny] = 2
                 queue.append((nx, ny))
+    return board
+
 
 flag = True
-while flag:
+answer = 0
+while (flag):
     flag = False
-    isAround(0, 0)
+    board = isOutSide(board)
     for i in range(N):
         for j in range(M):
-            count = 0
             if board[i][j] == 1:
-                flag = True
+                cnt = 0
                 for k in range(4):
                     nx = i+dx[k]
                     ny = j+dy[k]
                     if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 2:
-                        count += 1
-            if count >= 2:
-                board[i][j] = 'C'
+                        cnt += 1
+                if cnt >= 2:
+                    board[i][j] = 0
+                    flag = True
     for i in range(N):
         for j in range(M):
-            if board[i][j] == 'C' or board[i][j] == 2:
+            if board[i][j] == 2:
                 board[i][j] = 0
     if flag == False:
         break
-    ans += 1
+    answer += 1
 
-print(ans)
+print(answer)
