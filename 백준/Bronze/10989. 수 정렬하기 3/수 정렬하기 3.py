a18c0536194a473e383873ef6d0b8b2171f8298f
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-03-09 16:44:49 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8296 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,13 +1,19 @@
 import sys
+
+#sys.stdin = open("input.txt", "r")
+
 input = sys.stdin.readline
 
 N = int(input())
 arr = [0 for _ in range(10001)]
 
-for _ in range(N):
-    arr[int(input())] += 1
+for i in range(N):
+    num = int(input())
+    arr[num]+=1
 
-for i in range(1, len(arr)):
-    if arr[i] != 0:
-        for _ in range(arr[i]):
-            print(i)
+for i in range(len(arr)):
+    if arr[i] == 0:
+        continue
+    
+    for _ in range(arr[i]):
+        print(i)
