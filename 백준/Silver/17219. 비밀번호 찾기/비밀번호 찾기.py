da16db497072575c83291c50d658b9c1cf5409d2
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-03-11 21:58:53 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 264 ms, Memory: 56440 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,11 +1,22 @@
+import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
+
+#sys.stdin = open("input.txt", "r")
+
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
 my_dict = dict()
 
-[N, M] = list(map(int, input().split()))
+for _ in range(N):
+    address, password = map(str,input().split())
+    my_dict[address] = password
 
-for v in range(N):
-    [key, value] = list(map(str,input().split()))
-    my_dict[key] = value
-
-for v in range(M):
-    key = str(input())
-    print(my_dict[key])
+for _ in range(M):
+    print(my_dict[str(input().rstrip())])
