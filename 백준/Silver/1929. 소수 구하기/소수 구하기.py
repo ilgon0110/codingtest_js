1d940d98d56be2f93eb5fc4dc68a28f6cb3195ab
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-03-11 18:29:19 +0900

    [Silver III] Title: 소수 구하기, Time: 3296 ms, Memory: 40564 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,29 @@
+import sys
+import collections
 import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
-[N, M] = list(map(int, input().split()))
-for i in range(N, M+1):
-    if i == 1:
-        continue
-    for j in range(2, int(math.sqrt(i))+1):
-        if i % j == 0:
-            break
-    else:
+#sys.stdin = open("input.txt", "r")
+
+input = sys.stdin.readline
+
+M,N = map(int,input().split())
+
+def isPrime(num):
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+    for i in range(2, math.ceil(math.sqrt(num))+1):
+        if num%i == 0:
+            return False
+    
+    return True
+
+for i in range(M,N+1):
+    if isPrime(i):
         print(i)
