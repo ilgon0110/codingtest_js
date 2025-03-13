8babb2ad61a9d565302ffb0b3fb752ce2f985c00
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-03-13 18:08:14 +0900

    [Silver II] Title: 유기농 배추, Time: 244 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,34 +1,60 @@
 import sys
+import collections
+import math
 from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
+
+#sys.stdin = open("input.txt", "r")
 
 T = int(input())
 
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
+
 for _ in range(T):
-    [M, N, K] = list(map(int, input().split()))
-
-    board = [list(0 for _ in range(M)) for _ in range(N)]
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
-
-    for _ in range(K):
-        [m, n] = list(map(int, input().split()))
-        board[n][m] = 1
-
-    count = 0
+    M,N,K = map(int,input().split())
+    board = [[0 for _ in range(M)] for _ in range(N)]
+    visited = [[0 for _ in range(M)] for _ in range(N)]
+    for __ in range(K):
+        x,y = map(int,input().split())
+        board[y][x] = 1
+    cnt = 0
+    
+    def dfs(x,y):
+        board[x][y] = 0
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx < 0 or nx >=N or ny <0 or ny >= M:
+                continue
+            if board[nx][ny] == 0:
+                continue
+            board[nx][ny] = 0
+            dfs(nx,ny)
+    
+    def bfs(x,y):
+        queue = deque()
+        queue.append((x,y))
+        
+        while queue:
+            x,y = queue.popleft()
+            for i in range(4):
+                nx = x+dx[i]
+                ny = y+dy[i]
+                if nx < 0 or nx >=N or ny <0 or ny >= M:
+                    continue
+                if board[nx][ny] == 0:
+                    continue
+                board[nx][ny] = 0
+                queue.append((nx,ny))
+    
     for i in range(N):
         for j in range(M):
-            if (board[i][j] == 1):
-                count += 1
-                queue = deque()
-                queue.append([i, j])
-                board[i][j] = 0
-                while queue:
-                    [x, y] = queue.popleft()
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
-                        if (0 <= nx < N and 0 <= ny < M and board[nx][ny] == 1):
-                            board[nx][ny] = 0
-                            queue.append([nx, ny])
-
-    print(count)
+            if board[i][j] == 1:
+                bfs(i,j)
+                cnt+=1
+    
+    print(cnt)
