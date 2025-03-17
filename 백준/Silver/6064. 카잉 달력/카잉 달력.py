e9877dcc301361699edb610a41bc5063f605ce8c
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-03-17 13:58:07 +0900

    [Silver I] Title: 카잉 달력, Time: 4744 ms, Memory: 40412 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,30 +1,36 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
+
+#sys.stdin = open("input.txt", "r")
+
 input = sys.stdin.readline
 
 T = int(input())
 
 for _ in range(T):
-    M, N, x, y = map(int, input().split())
+    M,N,x,y = map(int,input().split())
+    ans = x
+    ny = x%N
+    if ny == 0:
+        ny = N
+    flag = ny == y
+    
+    while ny != y and ans <= (M*N):
+            ans += M
+            ny = (ny+M)%N
+            if ny == 0:
+                ny = N
+            if ny == y:
+                flag = True
 
-    cnt = x
-    front = x
-    back = 0
-
-    for i in range(x):
-        if back < N:
-            back += 1
-        else:
-            back = 1
-    flag = False
-
-    for _ in range(N):
-        if back == y:
-            print(cnt)
-            flag = True
-            break
-        back += M
-        cnt += M
-        while (back > N):
-            back -= N
-    if flag == False:
+    if flag:
+        print(ans)
+    else:
         print(-1)
+    
