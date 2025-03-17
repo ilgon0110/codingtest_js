dbd49b1634136a49d7946ca6274a3dc71f36fda2
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-03-17 18:14:07 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 40700 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,61 +1,65 @@
 import sys
+import collections
+import math
 from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
+
+#sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
 
 N = int(input())
 
-board = [list(input()) for _ in range(N)]
-diffBoard = []
-M = len(board[0])
+board = []
+redBoard = [[' ' for _ in range(N)] for _ in range(N)]
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+for _ in range(N):
+    board.append(list(map(str,input().rstrip())))
 
 for i in range(N):
-    tmp = []
-    for j in range(len(board[0])):
+    for j in range(N):
         if board[i][j] == 'G':
-            tmp.append('R')
+            redBoard[i][j] = 'R'
         else:
-            tmp.append(board[i][j])
-    diffBoard.append(tmp)
+            redBoard[i][j] = board[i][j]
 
-diffQueue = deque()
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
+
+def bfs(x,y,board,visited):
+    queue = deque()
+    queue.append((x,y))
+    
+    visited[x][y] = 1
+    
+    while queue:
+        x,y, = queue.popleft()
+        for k in range(4):
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if nx < 0 or nx >= N or ny < 0 or ny >= N:
+                continue
+            if visited[nx][ny] == 0 and board[nx][ny] == board[x][y]:
+                queue.append((nx,ny))
+                visited[nx][ny] = 1
+                
+visited = [[0 for _ in range(N)] for _ in range(N)]
 cnt = 0
 for i in range(N):
-    for j in range(M):
-        if board[i][j] != 0:
-            # bfs
-            cnt += 1
-            target = board[i][j]
-            queue = deque()
-            queue.append([i, j])
-            board[i][j] = 0
-            while queue:
-                [x, y] = queue.popleft()
-                for k in range(4):
-                    nx = x + dx[k]
-                    ny = y + dy[k]
-                    if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == target:
-                        board[nx][ny] = 0
-                        queue.append([nx, ny])
-diffCnt = 0
+    for j in range(N):
+        if visited[i][j] == 0:
+            bfs(i,j,board, visited)
+            cnt+=1
+            
+visited = [[0 for _ in range(N)] for _ in range(N)]
+cnt2 = 0
+
 for i in range(N):
-    for j in range(M):
-        if diffBoard[i][j] != 0:
-            # bfs
-            diffCnt += 1
-            target = diffBoard[i][j]
-            queue = deque()
-            queue.append([i, j])
-            diffBoard[i][j] = 0
-            while queue:
-                [x, y] = queue.popleft()
-                for k in range(4):
-                    nx = x + dx[k]
-                    ny = y + dy[k]
-                    if 0 <= nx < N and 0 <= ny < M and diffBoard[nx][ny] == target:
-                        diffBoard[nx][ny] = 0
-                        queue.append([nx, ny])
+    for j in range(N):
+        if visited[i][j] == 0:
+            bfs(i,j,redBoard,visited)
+            cnt2+=1
 
-
-print(cnt, diffCnt)
+print(cnt,cnt2)
