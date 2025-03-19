0eba8653ca13ba5ec98a571e22b7a82526f41dd2
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-03-19 21:02:48 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 352 ms, Memory: 58624 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,27 +1,52 @@
 import sys
-sys.setrecursionlimit(1000000)
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
+
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
 N = int(input())
-
 graph = [[] for _ in range(N+1)]
-parent = [1 for _ in range(N+1)]
-visited = [0 for _ in range(N+1)]
 for _ in range(N-1):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    x,y = map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+    
+parents = [1 for _ in range(N+1)]
+visited = [0 for _ in range(N+1)]
 
-
-def DFS(node):
+def dfs(node):
+    global parents, visited
+    visited[node] = 1
     for nextNode in graph[node]:
-        if visited[nextNode] > 0:
+        if visited[nextNode] == 1:
             continue
-        visited[node] = 1
-        parent[nextNode] = node
-        DFS(nextNode)
+        parents[nextNode] = node
+        dfs(nextNode)
 
-visited[1] = 1
-DFS(1)
-for i in range(2, len(parent)):
-    print(parent[i])
+def bfs(node):
+    global parents, visited
+    queue = deque()
+    visited[node] = 1
+    queue.append(node)
+    
+    while queue:
+        node = queue.popleft()
+        for nextNode in graph[node]:
+            if visited[nextNode] == 1:
+                continue
+            parents[nextNode] = node
+            visited[nextNode] = 1
+            queue.append(nextNode)
+
+for i in range(1,N+1):
+    if visited[i] == 0:
+        bfs(i)
+
+for i in range(2,N+1):
+    print(parents[i])
