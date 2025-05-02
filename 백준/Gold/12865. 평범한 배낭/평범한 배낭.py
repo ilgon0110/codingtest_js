9711e1fc46459f5631090ff999d015ed03feccf3
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-02 15:27:39 +0900

    [Gold V] Title: 평범한 배낭, Time: 2432 ms, Memory: 44420 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,26 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-
-dp = [[0 for _ in range(N+1)] for _ in range(K+1)]
-arr = [[0, 0]]
+N,K = map(int,input().split())
+items = []
 for _ in range(N):
-    arr.append(list(map(int, input().split())))
-for i in range(1, K+1):
-    for j in range(1, N+1):
-        weight, value = arr[j]
-        if i < weight:
-            dp[i][j] = dp[i][j-1]
-        else:
-            dp[i][j] = max(dp[i-weight][j-1]+value, dp[i][j-1])
+    W,V = map(int,input().split())
+    items.append((W,V))
 
-print(dp[K][N])
+dp = [0 for _ in range(K+1)]
+items.sort(key=lambda x : -x[0])
+
+for W,V in items:
+    for i in range(K,W-1,-1):
+        dp[i] = max(dp[i-W]+V,dp[i])
+
+print(max(dp))
