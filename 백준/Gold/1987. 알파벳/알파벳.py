8c05cd3d706b797560a2e2002de51c57379e92bc
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-02 23:25:06 +0900

    [Gold IV] Title: 알파벳, Time: 5072 ms, Memory: 153804 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,34 +1,38 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
-R, C = map(int, input().split())
+R,C = map(int,input().split())
 board = []
-
+visited = [[0 for _ in range(C)] for _ in range(R)]
 for _ in range(R):
-    board.append(list(input().rstrip()))
-
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-
+    board.append(list(str(input().rstrip())))
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
 ans = 0
-
 visited = [0 for _ in range(100)]
 
-def dfs(i, j, deps):
-    global ans
-    ans = max(ans, deps)
+def dfs(x,y,total):
+    global ans,visited
+    ans = max(total,ans)
     for k in range(4):
-        nx = i + dx[k]
-        ny = j + dy[k]
-        if nx >= 0 and nx < R and ny >= 0 and ny < C:
-            idx = ord(board[nx][ny])
-            if visited[idx] == 0:
-                visited[idx] = 1
-                dfs(nx, ny, deps+1)
-                visited[idx] = 0
-
-
+        nx = x+dx[k]
+        ny = y+dy[k]
+        if nx<0 or nx >= R or ny<0 or ny>=C:
+            continue
+        target = ord(board[nx][ny])
+        if visited[target] == 0:
+            visited[target] = 1
+            dfs(nx,ny,total+1)
+            visited[target] = 0
 visited[ord(board[0][0])] = 1
-dfs(0, 0, 1)
+dfs(0,0,1)
 print(ans)
