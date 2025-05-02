6fc0069abfbaec592ade1c350398918d7dbb471b
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-02 16:10:37 +0900

    [Gold V] Title: 치킨 배달, Time: 276 ms, Memory: 40504 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,62 +1,71 @@
 import sys
+import collections
+import math
 from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 N,M = map(int,input().split())
-board = []
-for _ in range(N):
-    board.append(list(map(int,input().split())))
-
 dx = [-1,0,1,0]
 dy = [0,-1,0,1]
+board = [[0 for _ in range(N+1)]]
+for _ in range(N):
+    board.append([0]+list(map(int,input().split())))
 
-def get_dist(chickens):
-    dists = [[sys.maxsize for _ in range(N)] for _ in range(N)]
-    for x,y in chickens:
-        queue = deque()
-        visited = [[0 for _ in range(N)] for _ in range(N)]
-        queue.append((x,y,1))
-
-        while queue:
-            x,y,dist = queue.popleft()
-
-            for k in range(4):
-                nx = x+dx[k]
-                ny = y+dy[k]
-                if 0<=nx<N and 0<=ny<N and board[nx][ny] == 1:
-                    dists[nx][ny] = min(dists[nx][ny] , dist)
-
-                if 0<=nx<N and 0<=ny<N and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
-                    queue.append((nx,ny,dist+1))
-    total = 0
-    for x,y in citys:
-        total += dists[x][y]
-
-    return total
-
-
-chicken_house = []
-citys = []
-for i in range(N):
-    for j in range(N):
+chickenStores = []
+homes = []
+for i in range(N+1):
+    for j in range(N+1):
         if board[i][j] == 2:
-            chicken_house.append((i,j))
+            chickenStores.append((i,j))
         if board[i][j] == 1:
-            citys.append((i,j))
+            homes.append((i,j))
 
-answer = sys.maxsize
-def recursive(L,s,tmp):
+def chickenDistance(x,y,cx,cy):
+    return abs(x-cx)+abs(y-cy)
+
+def findChickenStore(x,y):
+    visited = [[0 for _ in range(N+1)] for _ in range(N+1)]
+    queue = deque()
+    queue.append((x,y))
+    visited[x][y] = 1
+    
+    while queue:
+        x,y = queue.popleft()
+        if board[x][y] == 2:
+            return (x,y)
+        for k in range(4):
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if nx<1 or nx>=N+1 or ny<1 or ny>=N+1:
+                continue
+            if visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                queue.append((nx,ny))
+
+ans = sys.maxsize
+def dfs(L,s,track):
     if L == M:
-        global  answer
-        total = get_dist(tmp)
-        answer = min(total,answer)
+        global ans
+        chickens = track
+        total = 0
+        for x,y in homes:
+            minDist = sys.maxsize
+            for cx,cy in chickens:
+                minDist = min(chickenDistance(x,y,cx,cy), minDist)
+            total += minDist
+        ans = min(ans,total)
         return
     else:
-        for i in range(s,len(chicken_house)):
-            tmp.append(chicken_house[i])
-            recursive(L+1,i+1,tmp)
-            tmp.pop()
+        for i in range(s,len(chickenStores)):
+            track.append(chickenStores[i])
+            dfs(L+1,i+1,track)
+            track.pop()
 
-recursive(0,0,[])
-print(answer)
+
+dfs(0,0,[])
+print(ans)
