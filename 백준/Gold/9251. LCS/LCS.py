1fcd7d9de5786bc972df6cfe15f346d03eff124d
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-02 15:03:11 +0900

    [Gold V] Title: LCS, Time: 772 ms, Memory: 64800 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,21 +1,29 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
-A = list((input().rstrip()))
-B = list((input().rstrip()))
 
-LCS = [[0 for _ in range(len(A)+1)] for _ in range(len(B)+1)]
+text1 = str(input().rstrip())
+text2 = str(input().rstrip())
 
-for i in range(1, len(B)+1):
-    for j in range(1, len(A)+1):
-        if A[j-1] == B[i-1]:
-            LCS[i][j] = LCS[i-1][j-1] + 1
-        else:
-            LCS[i][j] = max(LCS[i][j-1], LCS[i-1][j])
+N = len(text1)
+M = len(text2)
 
+dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
 ans = 0
-for i in range(1, len(B)+1):
-    for j in range(1, len(A)+1):
-        ans = max(ans, LCS[i][j])
+for i in range(1,N+1):
+    for j in range(1,M+1):
+        if text2[j-1] == text1[i-1]:
+            dp[i][j] = max(dp[i-1][j-1]+1,dp[i-1][j],dp[i][j-1])
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        ans = max(ans,dp[i][j])
 
 print(ans)
