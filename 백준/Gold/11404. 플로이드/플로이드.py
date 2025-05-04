6747f14d8dfb66e5e66425feb83d3699827c3443
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-04 17:19:23 +0900

    [Gold IV] Title: 플로이드, Time: 344 ms, Memory: 49236 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,37 +1,40 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-
 graph = [[] for _ in range(N+1)]
-
 for _ in range(M):
-    a, b, c = map(int, input().split())
-    graph[a].append((c, b))
+    a,b,c = map(int,input().split())
+    graph[a].append((c,b))
 
 dist = [[sys.maxsize for _ in range(N+1)] for _ in range(N+1)]
 
-for i in range(1, N+1):
+for i in range(1,N+1):
     dist[i][i] = 0
-    for weight, to in graph[i]:
-        dist[i][to] = min(dist[i][to], weight)
+    for cost,to in graph[i]:
+        dist[i][to] = min(dist[i][to], cost)
 
-def floyd():
-    for k in range(1, N+1):
-        for i in range(1, N+1):
-            for j in range(1, N+1):
-                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+def floid():
+    for k in range(1,N+1):
+        for i in range(1,N+1):
+            for j in range(1,N+1):
+                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
+                    
+floid()
 
-
-floyd()
-
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        if dist[i][j] == sys.maxsize:
-            dist[i][j] = 0
-
-for i in range(1, N+1):
-    ans = dist[i]
-    print(*ans[1:len(ans)])
+for i in range(1,N+1):
+    tmp = dist[i][1:]
+    for j in range(len(tmp)):
+        if tmp[j] == sys.maxsize:
+            tmp[j] = 0
+    print(*tmp)
