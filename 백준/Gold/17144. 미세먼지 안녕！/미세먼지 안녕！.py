b60b66933fb91a38fb7778a07eb49b6a1fd5022d
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-04 22:36:53 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 464 ms, Memory: 120460 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,102 +1,136 @@
 import sys
+import collections
 import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
-R, C, T = map(int, input().split())
-
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+R,C,T = map(int,input().split())
 board = []
 for _ in range(R):
-    board.append(list(map(int, input().split())))
+    board.append(list(map(int,input().split())))
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
 
-
-def airFreshUp(x1):
-    arr = []
-    for j in range(1, C):
-        arr.append(board[x1][j])
-    for i in range(x1-1, -1, -1):
-        arr.append(board[i][C-1])
-    for j in range(C-2, -1, -1):
-        arr.append(board[0][j])
-    for i in range(1, x1-1):
-        arr.append(board[i][0])
-    board[x1][1] = 0
-    idx = 0
-    for j in range(2, C):
-        board[x1][j] = arr[idx]
-        idx += 1
-    for i in range(x1-1, -1, -1):
-        board[i][C-1] = arr[idx]
-        idx += 1
-    for j in range(C-2, -1, -1):
-        board[0][j] = arr[idx]
-        idx += 1
-    for i in range(1, x1):
-        board[i][0] = arr[idx]
-        idx += 1
-
-
-def airFreshDown(x2):
-    arr = []
-    for j in range(1, C):
-        arr.append(board[x2][j])
-    for i in range(x2+1, R):
-        arr.append(board[i][C-1])
-    for j in range(C-2, -1, -1):
-        arr.append(board[R-1][j])
-    for i in range(R-2, x2+1, -1):
-        arr.append(board[i][0])
-    idx = 0
-    board[x2][1] = 0
-    for j in range(2, C):
-        board[x2][j] = arr[idx]
-        idx += 1
-    for i in range(x2+1, R):
-        board[i][C-1] = arr[idx]
-        idx += 1
-    for j in range(C-2, -1, -1):
-        board[R-1][j] = arr[idx]
-        idx += 1
-    for i in range(R-2, x2, -1):
-        board[i][0] = arr[idx]
-        idx += 1
-
-
-airBox = []
-for i in range(R):
-    for j in range(C):
-        if board[i][j] == -1:
-            airBox.append(i)
 for _ in range(T):
-    visited = [[0 for _ in range(C)] for _ in range(R)]
-
+    tmp = [[0 for _ in range(C)] for _ in range(R)]
+    targets = []
+    cleaner  = []
     for i in range(R):
         for j in range(C):
             if board[i][j] > 0:
-                cnt = 0
-                for k in range(4):
-                    nx = i + dx[k]
-                    ny = j + dy[k]
-                    if 0 <= nx < R and 0 <= ny < C:
-                        if board[nx][ny] == -1:
-                            continue
-                        visited[nx][ny] += math.floor(board[i][j]/5)
-                        cnt += 1
-                board[i][j] -= (math.floor(board[i][j]/5))*cnt
+                targets.append((i,j))
+            if board[i][j] == -1:
+                cleaner.append((i,j))
+    
+    for x,y in targets:
+        cnt = 0
+        for k in range(4):
+            nx = x+dx[k]
+            ny = y+dy[k]
+            if nx<0 or nx>=R or ny<0 or ny>=C:
+                continue
+            if board[nx][ny] == -1:
+                continue
+            tmp[nx][ny] += board[x][y]//5
+            cnt+=1
+        board[x][y] = board[x][y] - ((board[x][y]//5)*cnt)
+    
     for i in range(R):
         for j in range(C):
-            board[i][j] += visited[i][j]
-    airFreshUp(airBox[0])
-    airFreshDown(airBox[1])
+            board[i][j] += tmp[i][j]
+    
+    # 공기청청기 작동
+    x1,y1 = cleaner[0]
+    queue1 = deque()
+    for i in range(1,C):
+        queue1.append(board[x1][i])
+    tmp1 = queue1.pop()
+    queue1.appendleft(0)
 
+    for i in range(1,C):
+        board[x1][i] = queue1[i-1]
+    
+    queue2 = deque()
+    for i in range(x1-1,-1,-1):
+        queue2.append(board[i][C-1])
+    queue2.appendleft(tmp1)
+    tmp2 = queue2.pop()
 
+    tmpIdx = 0
+    for i in range(x1-1,-1,-1):
+        board[i][C-1] = queue2[tmpIdx]
+        tmpIdx+=1
+        
+    
+    queue3 = deque()
+    for i in range(C-2,-1,-1):
+        queue3.append(board[0][i])
+    queue3.appendleft(tmp2)
+    tmp3 = queue3.pop()
+    tmpIdx = 0
+    for i in range(C-2,-1,-1):
+        board[0][i] = queue3[tmpIdx]
+        tmpIdx+=1
+    
+    queue4 = deque()
+    for i in range(1,x1):
+        queue4.append(board[i][0])
+    queue4.appendleft(tmp3)
+    queue4.pop()
+    for i in range(1,x1):
+        board[i][0] = queue4[i-1]
+    
+    x2,y2 = cleaner[1]
+    queue = deque()
+    for i in range(1,C):
+       queue.append(board[x2][i])
+    tmp = queue.pop()
+    queue.appendleft(0)
+    tmpIdx = 0
+    for i in range(1,C):
+        board[x2][i] = queue[tmpIdx]
+        tmpIdx +=1
+
+    queue=deque()
+    for i in range(x2+1,R):
+        queue.append(board[i][C-1])
+    queue.appendleft(tmp)
+    tmp = queue.pop()
+    tmpIdx = 0
+    for i in range(x2+1,R):
+        board[i][C-1] = queue[tmpIdx]
+        tmpIdx+=1
+    
+    queue=deque()
+    for i in range(C-2,-1,-1):
+        queue.append(board[R-1][i])
+    queue.appendleft(tmp)
+    tmp = queue.pop()
+    tmpIdx = 0
+    for i in range(C-2,-1,-1):
+        board[R-1][i] = queue[tmpIdx]
+        tmpIdx+=1
+    
+    queue = deque()
+    for i in range(R-2,x2,-1):
+        queue.append(board[i][0])
+    queue.appendleft(tmp)
+    queue.pop()
+    tmpIdx = 0
+    for i in range(R-2,x2,-1):
+        board[i][0] = queue[tmpIdx]   
+        tmpIdx+=1
+    
 ans = 0
 for i in range(R):
     for j in range(C):
-        if board[i][j] == -1:
-            continue
-        ans += board[i][j]
+        if board[i][j] > 0:
+            ans += board[i][j]
 
 print(ans)
