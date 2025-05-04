aaa0033cd12453b5e687f21944aef2fb1f45cc4d
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-04 14:32:58 +0900

    [Gold IV] Title: N-Queen, Time: 28364 ms, Memory: 202520 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,30 +1,40 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
 N = int(input())
-
+board = [[0 for _ in range(N)] for _ in range(N)]
 arr = [0]*N
-ans = 0
 
 def is_possible(row):
     for i in range(row):
+        # 같은 세로줄에 위치
         if arr[row] == arr[i]:
             return False
-        if abs(row - i) == abs(arr[row] - arr[i]):
+        # 대각 방향에 위치
+        if(abs(row-i) == abs(arr[row]-arr[i])):
             return False
     return True
-
-def DFS(row):
+        
+ans = 0
+def dfs(row):
     if row == N:
         global ans
-        ans += 1
+        ans +=1
         return
     else:
         for i in range(N):
             arr[row] = i
             if is_possible(row):
-                DFS(row+1)
+                dfs(row+1)
 
-DFS(0)
+dfs(0)
 print(ans)
