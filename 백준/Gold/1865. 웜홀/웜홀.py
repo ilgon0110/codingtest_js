c22766b10af1b32fa7a7130177872a731b623db4
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-05 15:31:14 +0900

    [Gold III] Title: 웜홀, Time: 1040 ms, Memory: 40884 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,34 +1,58 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
+import heapq
+
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
-TC = int(input())
-INF = sys.maxsize
+def floid(dist):
+    newDist = [[0 for _ in range(N+1)] for _ in range(N+1)]
+    for i in range(N+1):
+        for j in range(N+1):
+            newDist[i][j] = dist[i][j]
+            
+    for k in range(1,N+1):
+        for i in range(1,N+1):
+            for j in range(1,N+1):
+                if newDist[i][j] > newDist[i][k]+newDist[k][j]:
+                    newDist[i][j] = newDist[i][k]+newDist[k][j]
+        
+    return newDist
 
-def velman_ford(start):
+def vellman(start):
+    dist = [sys.maxsize for _ in range(N+1)]
     dist[start] = 0
-    for check in range(N):
-        for node in range(1, N+1):
-            for weight, to in graph[node]:
-                if dist[to] > dist[node] + weight:
-                    dist[to] = dist[node] + weight
-                    if check == N-1:
-                        return False
-    return True
+    
+    for k in range(N):
+        for i in range(1,N+1):
+            for cost,to in graph[i]:
+                if dist[to] > dist[i] + cost:
+                    if k == N-1:
+                        return True
+                    dist[to] = dist[i]+cost
+    return False
+                    
 
-
+TC = int(input())
 for _ in range(TC):
-    N, M, W = map(int, input().split())
+    N,M,W = map(int,input().split())
     graph = [[] for _ in range(N+1)]
     for _ in range(M):
-        S, E, T = map(int, input().split())
-        graph[S].append((T, E))
-        graph[E].append((T, S))
+        s,e,t = map(int,input().split())
+        graph[s].append((t,e))
+        graph[e].append((t,s))
     for _ in range(W):
-        S, E, T = map(int, input().split())
-        graph[S].append((-T, E))
-
-    dist = [INF for _ in range(N+1)]
-    if velman_ford(1) == True:
-        print('NO')
-    else:
+        s,e,t = map(int,input().split())
+        graph[s].append((-t,e))
+    
+    flag = vellman(1)
+    
+    if flag:
         print('YES')
+    else:
+        print('NO')
