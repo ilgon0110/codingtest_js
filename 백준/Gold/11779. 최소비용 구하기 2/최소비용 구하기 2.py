1ee6980382843b0091ea15b03925c7f163716f29
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-05-05 16:20:59 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 272 ms, Memory: 72400 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,38 +1,40 @@
 import sys
+import collections
+import math
+from collections import deque
+import copy
+import bisect
+import itertools
 import heapq
 
+#sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
 graph = [[] for _ in range(N+1)]
 for _ in range(M):
-    a, b, c = map(int, input().split())
-    graph[a].append((c, b))
+    a,b,c = map(int,input().split())
+    graph[a].append((c,b))
 
-start, end = map(int, input().split())
-dist = [sys.maxsize for _ in range(N+1)]
+start,end = map(int,input().split())
 
-
-def dijkstra(start):
+def dijkstra(start,end):
+    dist = [sys.maxsize for _ in range(N+1)]
     dist[start] = 0
     queue = []
-    heapq.heappush(queue, [0, start, [start]])
-
+    heapq.heappush(queue,(0,start,[start]))
+    
     while queue:
-        [weight, node, track] = heapq.heappop(queue)
+        cost,node,track = heapq.heappop(queue)
         if node == end:
-            return dist[end], track
-        for nextWeight, to in graph[node]:
-            cost = weight + nextWeight
-            if dist[to] <= cost:
-                continue
-            dist[to] = cost
-            heapq.heappush(queue, [cost, to, track + [to]])
+            return (dist[node],track)
+        for weight,to in graph[node]:
+            if dist[to] > cost+weight:
+                dist[to] = cost+weight
+                heapq.heappush(queue,(cost+weight,to,track+[to]))
 
-
-answer, list = dijkstra(start)
-
-print(answer)
-print(len(list))
-print(*list)
+time, track = dijkstra(start,end)
+print(time)
+print(len(track))
+print(*track)
