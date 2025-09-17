748a5f2ed788626935a1848d37fe043a554744b7
Kim ilgon <82035356+ilgon0110@users.noreply.github.com>
2025-09-17 22:11:24 +0900

    [Gold III] Title: 파일 합치기, Time: 2292 ms, Memory: 122348 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -10,26 +10,26 @@ import heapq
 #sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
+# 다시 풀어보기
 T = int(input())
-
 for _ in range(T):
     K = int(input())
     files = list(map(int,input().split()))
-    subtotal = [0 for _ in range(K+1)]
     
-    for i in range(1,K+1):
-        subtotal[i] = files[i-1]
-        subtotal[i] += subtotal[i-1]
-    
-    # dp[i][j] = i에서 j까지 합친 최소값 저장
     dp = [[sys.maxsize for _ in range(K)] for _ in range(K)]
-    
+    subtotal = [0 for _ in range(K+1)]
+    subtotal[1] = files[0]
+    for i in range(1,K):
+        subtotal[i+1] = subtotal[i]
+        subtotal[i+1] += files[i]
+
     for i in range(K):
         dp[i][i] = 0
     
-    for i in range(2,K+1): #길이
-        for j in range(K-i+1): #시작 인덱스
-            for k in range(j,j+i-1): #하나하나씩
+    for i in range(2,K+1):
+        for j in range(K-i+1):
+            for k in range(j,j+i-1):
+                #print(i,j,k)
                 dp[j][j+i-1] = min(dp[j][j+i-1], dp[j][k]+dp[k+1][j+i-1]+subtotal[j+i]-subtotal[j])
-    
+
     print(dp[0][K-1])
